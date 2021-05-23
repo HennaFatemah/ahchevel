af8f57d6c850c1e5a55ef5b019f4f9fdb3bced75
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-23 04:16:16 -0400

    app as a functional component created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from './assets/logo/logo.png';
 import './App.scss';
+import data from './data.json';
 
-const App = () => {
-  return (
-    <div className="grid-container">
-      <div className="grid-container__head">
-        <button className="grid-container__logo"><img className="grid-container__logo-pic" src={Logo} alt="" /></button>
+class App extends Component {
+  render() {
+    return (
+      <div className="grid-container">
+        <div className="grid-container__head">
+          <button className="grid-container__logo"><img className="grid-container__logo-pic" src={Logo} alt="" /></button>
+        </div>
+        <div className="grid-container__main">
+          Jewelry List
+        </div>
+        <div className="grid-container__foot">
+          All Right Is Reserved
+        </div>
       </div>
-      <div className="grid-container__main">
-        Jewelry List
-      </div>
-      <div className="grid-container__foot">
-        All Right Is Reserved
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
