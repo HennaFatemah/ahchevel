34f183e021d67fca73f26087f2f1a7ed1f721925
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-23 05:37:02 -0400

    sort and filter component created

---
+++ src/components/SortAndFilter/SortAndFilter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SortAndFilter.scss';
+
+class SortAndFilter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SortAndFilter;
