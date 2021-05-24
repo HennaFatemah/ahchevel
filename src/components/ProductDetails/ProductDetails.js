8dc14c995fa26e26703a2af43afe56fca0b51aa3
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-24 12:04:31 -0400

    product details styled

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -5,20 +5,20 @@ import './ProductDetails.scss'
 const ProductDetails = ({ product, addToCart, closeWindow }) => {
     return (
         <div className="details">
+            <div className="details__shadow"  onClick={()=>closeWindow()}></div>
             <div className="details__window">
                 <button className="details__btn" onClick={()=>closeWindow()}>x</button>
                 <Slider 
-                    className="details__part" 
                     imageMain = {product.image}
                     imageOne = {product.imageOne}
                     imageTwo = {product.imageTwo}
                     imageThree = {product.imageThree}
                 />
-                <div>
-                    <p>{product.title}</p>
-                    <p>{product.category}</p>
-                    <p>{product.description}</p>
-                    <p>${product.price}</p>
+                <div className="details__contents">
+                    <p className="details__text">{product.title}</p>
+                    <p className="details__text">Category: "{product.category.charAt(0).toUpperCase() + product.category.slice(1)}"</p>
+                    <p className="details__text">{product.description}</p>
+                    <p className="details__text">${product.price}</p>
                     <button
                         className="details__btn"
                         onClick={()=>{
