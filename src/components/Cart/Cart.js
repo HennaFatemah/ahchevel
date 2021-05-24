34c92ac62b0c0b8e59389e93afca538d33efcb1e
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-23 20:59:51 -0400

    checkout form completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,44 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CheckoutForm from '../CheckoutForm/CheckoutForm';
 import './Cart.scss';
 
-const Cart = ({ cartItems, removeFromCart }) => {
-    return (
-        <div className="cart">
-            <div className="cart__items">
-                {cartItems.length === 0 ? 
-                    <div className="cart__head">Cart is empty</div>:
-                    cartItems.length === 1 ? 
-                    <div className="cart__head">You have {cartItems.length} item in the cart</div> :
-                    <div className="cart__head">You have {cartItems.length} items in the cart</div>
+class Cart extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email:'',
+            name:'',
+            address:'',
+            showCheckout: false,
+        }
+    }
+
+    onProceed = () => {
+        this.setState({showCheckout: true})
+    }
+
+    handleEmailInput = (e) => {
+        this.setState({email: e.target.value})
+    }
+
+    handleNameInput = (e) => {
+        this.setState({name: e.target.value})
+    }
+
+    handleAddressInput = (e) => {
+        this.setState({address: e.target.value})
+    }
+
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            email: this.state.email,
+            name: this.state.name,
+            address: this.state.address,
+            cartItems: this.props.cartItems,
+        }
+        this.props.createOrder(order)
+    }
+
+    render() {
+        const { cartItems, removeFromCart } = this.props;
+        return (
+            <div className="cart">
+                <div className="cart__items">
+                    {cartItems.length === 0 ? 
+                        <div className="cart__head">Cart is empty</div>:
+                        cartItems.length === 1 ? 
+                        <div className="cart__head">You have {cartItems.length} item in the cart</div> :
+                        <div className="cart__head">You have {cartItems.length} items in the cart</div>
+                    }
+                </div>
+                <ul className="cart__items">
+                    {cartItems.map(item => {
+                        return(
+                            <li className="cart__card" key={item.id}>
+                                <div className="cart__left">
+                                    <img className="cart__pic" src={item.image} alt={item.title} />
+                                </div>
+                                <div className="cart__right">
+                                    <p className="cart__title">{item.title}</p>
+                                    <div className="cart__bottom">
+                                        <p className="cart__title">{item.count} x ${item.price}</p>
+                                        <button className="cart__btn" onClick={()=> removeFromCart(item)}>Remove</button>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+                {cartItems.length !== 0 && 
+                    <div className="cart__total">
+                        <p className="cart__calc">Total: ${cartItems.reduce((a, b)=> a + b.price * b.count, 0)}</p>
+                        <button className="cart__btn" onClick={this.onProceed}>Proceed</button>
+                    </div>
+                }
+                {this.state.showCheckout &&
+                    <CheckoutForm
+                        onProceed={this.state.showCheckout}
+                        createOrder={this.createOrder}
+                        handleEmailInput={this.handleEmailInput}
+                        handleNameInput={this.handleNameInput}
+                        handleAddressInput={this.handleAddressInput}
+
+                    />
                 }
             </div>
-            <ul className="cart__items">
-                {cartItems.map(item => {
-                    return(
-                        <li className="cart__card" key={item.id}>
-                            <div className="cart__left">
-                                <img className="cart__pic" src={item.image} alt={item.title} />
-                            </div>
-                            <div className="cart__right">
-                                <p className="cart__title">{item.title}</p>
-                                <div className="cart__bottom">
-                                    <p className="cart__title">{item.count} x ${item.price}</p>
-                                    <button className="cart__btn" onClick={()=> removeFromCart(item)}>Remove</button>
-                                </div>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-            {cartItems.length !== 0 && 
-                <div className="cart__total">
-                    <p className="cart__calc">Total: ${cartItems.reduce((a, b)=> a + b.price * b.count, 0)}</p>
-                    <button className="cart__btn">Proceed</button>
-                </div>
-            }
-            
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Cart;
