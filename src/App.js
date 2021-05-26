4d6896513373cf1c715fee3ae8959820260ba78b
Henna Fatemah <henna_fatemah@yahoo.com>
2021-05-25 23:54:11 -0400

    routes added

--- src/App.js
+++ src/App.js
@@ -6,30 +6,35 @@ import SortAndFilter from './components/SortAndFilter/SortAndFilter';
 import Cart from './components/Cart/Cart';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="grid-container">
-          <div className="grid-container__head">
-            <button className="grid-container__logo"><img className="grid-container__logo-pic" src={Logo} alt="" /></button>
-          </div>
-          <div className="grid-container__main">
-            <div className="grid-container__content">
-              <div className="grid-container__products">
-                <SortAndFilter/>
-                <Products/>
-              </div>
-              <div className="grid-container__cart">
-                <Cart/>
+        <BrowserRouter>
+          <div className="grid-container">
+            <div className="grid-container__head">
+              <Link to="/" className="grid-container__logo"><img className="grid-container__logo-pic" src={Logo} alt="" /></Link>
+            </div>
+            <div className="grid-container__main">
+            <Route path="/admin" component={Admin}/>
+              <Route path="/" component={HomeScreen} exact/>
+              <div className="grid-container__content">
+                <div className="grid-container__products">
+                  <SortAndFilter/>
+                  <Products/>
+                </div>
+                <div className="grid-container__cart">
+                  <Cart/>
+                </div>
               </div>
             </div>
+            <div className="grid-container__foot">
+              All Right Is Reserved
+            </div>
           </div>
-          <div className="grid-container__foot">
-            All Right Is Reserved
-          </div>
-        </div>
+        </BrowserRouter>
       </Provider>
     );
   }
